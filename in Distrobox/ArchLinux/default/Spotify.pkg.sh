c8afa1ffae057b8f9ff6b363beca068af313c874
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-06-03 09:00:38 -0500

    Update Spotify.pkg.sh

--- in Distrobox/ArchLinux/default/Spotify.pkg.sh
+++ in Distrobox/ArchLinux/default/Spotify.pkg.sh
@@ -1,4 +1,5 @@
 app=Spotify
+bin=spotify-launcher
 
 distrobox create --name $app --home ~/.uni/hyper/distrobox/HOME/$app --image quay.io/toolbx/arch-toolbox:latest
 
@@ -11,11 +12,11 @@ installYay(){
 
 sudo pacman -Sy
 
-sudo pacman -S spotify-launcher pamac
+sudo pacman -S $exe pamac
 
 yay -S spotblock-rs
 
 this=`echo "$app" | awk '{print tolower($0)}'`
 
 distrobox-export spotblock-rs
-distrobox-export --app $this
+distrobox-export --app $bin
