1dbe2d3a16ef9267cd31ce8ab6a740720ce3c6bb
Source Graph A.I. <117184625+UniInc@users.noreply.github.com>
2025-06-03 08:42:55 -0500

    Update Spotify.pkg.sh

--- in Distrobox/ArchLinux/default/Spotify.pkg.sh
+++ in Distrobox/ArchLinux/default/Spotify.pkg.sh
@@ -1,20 +1,22 @@
-app=steam
+app=Spotify
 
 distrobox create --name $app --home ~/.uni/hyper/distrobox/HOME/$app --image quay.io/toolbx/arch-toolbox:latest
 
 distrobox enter $app
 
-installYay(){
-  echo https://github.com/Unicorn-OS/ArchLinux.Knowledge/blob/main/Package%20Manager/AUR/Yay/readme.md
-}
+# installYay(){
+#   echo https://github.com/Unicorn-OS/ArchLinux.Knowledge/blob/main/Package%20Manager/AUR/Yay/readme.md
+# }
 
-echo """
-[multilib]
-Include = /etc/pacman.d/mirrorlist
-""" | sudo tee -a /etc/pacman.conf
+# echo """
+# [multilib]
+# Include = /etc/pacman.d/mirrorlist
+# """ | sudo tee -a /etc/pacman.conf
 
 sudo pacman -Sy
 
-sudo pacman -S steam
+sudo pacman -S spotify-launcher pamac
+
+pamac build spotblock-rs
 
 distrobox-export --app $app
